12b839210eff9bb698238cb4cbc0a325bca447bb
sebasroi <sroizenberg@gmail.com>
2023-07-27 15:44:59 -0300

    para visualizar en gitpages

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -11,6 +11,15 @@ export const GifGrid = ( { category }) => {
   return (
     <>
         <h3>{ category }</h3>
+
+        {
+          isLoading && (<h2>Cargando...</h2>)
+        }
+
+{/*         {
+          isLoading ? <h2>Cargando...</h2> : null
+        } */}
+
         <div className="card-grid">          
           {
           images.map( ( image ) => (
